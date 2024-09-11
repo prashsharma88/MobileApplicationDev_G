c715dc4030ff7dc2c4da8b7aebcfe7566e8136b1
prashsharma88 <prashant.sharma@sait.ca>
2024-09-10 22:44:56 -0600

    adding UIDemo project

---
+++ UIDemo/src/Main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+    StyleSheet,
+    SafeAreaView
+} from "react-native";
+
+
+const Main = () : React.JSX.Element => {
+    return (
+        <SafeAreaView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    app_bg: {
+        backgroundColor: '#DEE5D4'
+    }
+});
+
+
+export default Main;
